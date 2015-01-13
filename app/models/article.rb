33e5eb87bcdaadec43eab96a702a2c8888ceeb0a
Weyland Joyner <weylandjoyner4@gmail.com>
2015-01-13 18:29:33 -0500

    sample rails

---
+++ app/models/article.rb
@@ -0,0 +1,11 @@
+require 'elasticsearch/model'
+
+class Article < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+end
+
+Article.import
+
+# Q to consider - What does it mean for the article to be auto synced w/ Elasticsearch?
+# does this mean that SQL and Elasticsearch talk to each other?
