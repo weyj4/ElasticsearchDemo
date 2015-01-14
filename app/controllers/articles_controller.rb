4e6692ae92fcd52bbd1d1025b4fffec8545a75b8
Weyland Joyner <weylandjoyner4@gmail.com>
2015-01-14 12:41:15 -0500

    elasticsearch fully integrated and checked against SQL

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,12 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
   def show
     @article = Article.find(params[:id])
   end
--- app/models/article.rb
+++ app/models/article.rb
@@ -9,3 +9,16 @@ Article.import
 
 # Q to consider - What does it mean for the article to be auto synced w/ Elasticsearch?
 # does this mean that SQL and Elasticsearch talk to each other?
+
+def self.search(query)
+  __elasticsearch__.search(
+    {
+      query: {
+        multi_match: {
+        	query: query,
+        	fields: ['title^10', 'text']
+        }
+      }
+    }
+)
+end
